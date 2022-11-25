8e715158d2b2f17500df112078e38f14d9344ad8
Pedro Giampietro <pedrogiampietro@hotmail.com>
2022-11-25 16:47:03 -0300

    add: fix jwt

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,25 @@ import equipamentController from './controllers/EquipamentController'
 import ticketCategoryController from './controllers/TicketCategory'
 
 const app = express()
+
+app.use((_, response, next) => {
+	response.header('Access-Control-Allow-Origin', '*')
+	response.header(
+		'Access-Control-Allow-Methods',
+		'GET,HEAD,OPTIONS,POST,PUT,PATCH'
+	)
+	response.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	)
+	response.header('Access-Control-Expose-Headers', 'x-total-count')
+
+	return next()
+})
+
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/authenticate', authController)
 app.use('/account', userController)
