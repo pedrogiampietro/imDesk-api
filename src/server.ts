338d2ad0b32fa13a223535df9bc921b0ccfd23c1
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-10-25 12:34:51 -0300

    wrong path not running routes

--- src/server.ts
+++ src/server.ts
@@ -43,6 +43,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use("/", express.static(path.join(__dirname, "..", "build")));
 
 app.use("/authenticate", authController);
 app.use("/account", userController);
