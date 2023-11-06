5965c96b9d97c2a6c1e1efa34e29b4437bc28a68
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-11-06 15:32:21 -0300

    .

--- build/utils/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const client_1 = require("@prisma/client");
-const db = new client_1.PrismaClient();
-exports.db = db;
