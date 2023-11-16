916ff01b06ca7bc700e55b45765ab674cb4b0443
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-11-16 15:59:06 -0300

    add: fix shiftChange controller

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,45 +1,94 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const node_cron_1 = __importDefault(require("node-cron"));
-const AuthController_1 = __importDefault(require("./controllers/AuthController"));
-const UserController_1 = __importDefault(require("./controllers/UserController"));
-const TicketController_1 = __importDefault(require("./controllers/TicketController"));
-const EquipamentController_1 = __importDefault(require("./controllers/EquipamentController"));
-const TicketCategory_1 = __importDefault(require("./controllers/TicketCategory"));
-const TicketPriority_1 = __importDefault(require("./controllers/TicketPriority"));
-const TicketTypeController_1 = __importDefault(require("./controllers/TicketTypeController"));
-const LocationController_1 = __importDefault(require("./controllers/LocationController"));
-const MaintenanceController_1 = __importDefault(require("./controllers/MaintenanceController"));
-const ColletInformation_1 = __importDefault(require("./controllers/ColletInformation"));
-const ProviderController_1 = __importDefault(require("./controllers/ProviderController"));
-const CompanyController_1 = __importDefault(require("./controllers/CompanyController"));
+const AuthController_1 = __importDefault(
+  require("./controllers/AuthController")
+);
+const UserController_1 = __importDefault(
+  require("./controllers/UserController")
+);
+const TicketController_1 = __importDefault(
+  require("./controllers/TicketController")
+);
+const EquipamentController_1 = __importDefault(
+  require("./controllers/EquipamentController")
+);
+const TicketCategory_1 = __importDefault(
+  require("./controllers/TicketCategory")
+);
+const TicketPriority_1 = __importDefault(
+  require("./controllers/TicketPriority")
+);
+const TicketTypeController_1 = __importDefault(
+  require("./controllers/TicketTypeController")
+);
+const LocationController_1 = __importDefault(
+  require("./controllers/LocationController")
+);
+const MaintenanceController_1 = __importDefault(
+  require("./controllers/MaintenanceController")
+);
+const ColletInformation_1 = __importDefault(
+  require("./controllers/ColletInformation")
+);
+const ProviderController_1 = __importDefault(
+  require("./controllers/ProviderController")
+);
+const CompanyController_1 = __importDefault(
+  require("./controllers/CompanyController")
+);
 const SLAController_1 = __importDefault(require("./controllers/SLAController"));
-const DepositController_1 = __importDefault(require("./controllers/DepositController"));
-const DepositItemController_1 = __importDefault(require("./controllers/DepositItemController"));
-const GroupController_1 = __importDefault(require("./controllers/GroupController"));
-const TodooController_1 = __importDefault(require("./controllers/TodooController"));
-const EquipmentTypeController_1 = __importDefault(require("./controllers/EquipmentTypeController"));
-const GeneratePDFController_1 = __importDefault(require("./controllers/GeneratePDFController"));
-const ReportController_1 = __importDefault(require("./controllers/ReportController"));
+const DepositController_1 = __importDefault(
+  require("./controllers/DepositController")
+);
+const DepositItemController_1 = __importDefault(
+  require("./controllers/DepositItemController")
+);
+const GroupController_1 = __importDefault(
+  require("./controllers/GroupController")
+);
+const TodooController_1 = __importDefault(
+  require("./controllers/TodooController")
+);
+const EquipmentTypeController_1 = __importDefault(
+  require("./controllers/EquipmentTypeController")
+);
+const GeneratePDFController_1 = __importDefault(
+  require("./controllers/GeneratePDFController")
+);
+const ReportController_1 = __importDefault(
+  require("./controllers/ReportController")
+);
 const processQueue_1 = require("./services/processQueue");
 const app = (0, express_1.default)();
 app.use((_, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    response.header("Access-Control-Expose-Headers", "x-total-count");
-    return next();
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH"
+  );
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  response.header("Access-Control-Expose-Headers", "x-total-count");
+  return next();
 });
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "..", "uploads")));
+app.use(
+  "/uploads",
+  express_1.default.static(path_1.default.join(__dirname, "..", "uploads"))
+);
 app.use("/authenticate", AuthController_1.default);
 app.use("/account", UserController_1.default);
 app.use("/ticket", TicketController_1.default);
@@ -61,10 +110,10 @@ app.use("/equipmentType", EquipmentTypeController_1.default);
 app.use("/pdf-gen", GeneratePDFController_1.default);
 app.use("/report", ReportController_1.default);
 app.get("/", (req, res) => {
-    return res.json({ status: "OK", data: new Date().toLocaleString() });
+  return res.json({ status: "OK", data: new Date().toLocaleString() });
 });
 const port = process.env.PORT || 3333;
 node_cron_1.default.schedule("*/2 * * * *", processQueue_1.processEmailQueue);
 app.listen(port, () => {
-    console.log(`Server running at https://localhost:${port}/`);
+  console.log(`Server running at https://10.0.101.70:${port}/`);
 });
--- src/controllers/ShiftChangeController.ts
+++ src/controllers/ShiftChangeController.ts
@@ -1,167 +1,218 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post('/', async (request: Request, response: Response) => {
-	const {
-		date,
-		responsibleUserId,
-		temperatureControl,
-		companyId,
-		atribuidos,
-		planejados,
-		pendentes,
-	} = request.body;
+router.post("/", async (request: Request, response: Response) => {
+  const {
+    controleTemperatura,
+    companyId,
+    responsibleUserId,
+    atribuidos,
+    planejados,
+    pendentes,
+  } = request.body;
 
-	try {
-		const shiftChange = await prisma.shiftChange.create({
-			data: {
-				date,
-				responsibleUserId,
-				temperatureControl,
-				ShiftChangeCompanies: {
-					create: { companyId },
-				},
-			},
-		});
+  console.log("Dados recebidos:", {
+    controleTemperatura,
+    companyId,
+    responsibleUserId,
+    atribuidos,
+    planejados,
+    pendentes,
+  });
 
-		await prisma.shiftChange.update({
-			where: { id: shiftChange.id },
-			data: {
-				ShiftChangeAssignedTicket: {
-					connect: atribuidos
-						.filter((t: any) => t.id)
-						.map((ticket: any) => ({
-							shiftChangeId_ticketId: {
-								shiftChangeId: shiftChange.id,
-								ticketId: ticket.id,
-							},
-						})),
-				},
-				ShiftChangePlannedTicket: {
-					connect: planejados
-						.filter((t: any) => t.id)
-						.map((ticket: any) => ({
-							shiftChangeId_ticketId: {
-								shiftChangeId: shiftChange.id,
-								ticketId: ticket.id,
-							},
-						})),
-				},
-				ShiftChangePendingTicket: {
-					connect: pendentes
-						.filter((t: any) => t.id)
-						.map((ticket: any) => ({
-							shiftChangeId_ticketId: {
-								shiftChangeId: shiftChange.id,
-								ticketId: ticket.id,
-							},
-						})),
-				},
-			},
-		});
+  try {
+    // Cria uma nova ShiftChange
+    const shiftChange = await prisma.shiftChange.create({
+      data: {
+        date: new Date(),
+        responsibleUserId,
+        temperatureControl: controleTemperatura,
+        ShiftChangeCompanies: {
+          create: { companyId },
+        },
+      },
+    });
 
-		return response.status(200).json({
-			message: 'Shift Change created successfully',
-			body: shiftChange,
-			error: false,
-		});
-	} catch (err) {
-		console.error('Error on creating Shift Change: ', err);
-		return response
-			.status(500)
-			.json({ message: 'Internal server error', error: true });
-	}
+    console.log("ShiftChange criada:", shiftChange);
+
+    // Função para extrair IDs válidos dos tickets
+    const extractValidTicketIds = (tickets: any[]) => {
+      return tickets
+        .filter((ticket) => ticket.id && ticket.id.trim() !== "")
+        .map((ticket) => ticket.id);
+    };
+
+    // Extrai e associa os tickets à ShiftChange
+    const allTicketIds = [
+      ...extractValidTicketIds(atribuidos),
+      ...extractValidTicketIds(planejados),
+      ...extractValidTicketIds(pendentes),
+    ];
+
+    console.log("IDs dos tickets a serem associados:", allTicketIds);
+
+    // Associa os tickets atribuídos à ShiftChange
+    for (const ticket of atribuidos) {
+      if (ticket.id && ticket.id.trim() !== "") {
+        await prisma.shiftChangeAssignedTicket.create({
+          data: {
+            shiftChangeId: shiftChange.id,
+            ticketId: ticket.id,
+          },
+        });
+      }
+    }
+
+    // Associa os tickets planejados à ShiftChange
+    for (const ticket of planejados) {
+      if (ticket.id && ticket.id.trim() !== "") {
+        await prisma.shiftChangePlannedTicket.create({
+          data: {
+            shiftChangeId: shiftChange.id,
+            ticketId: ticket.id,
+          },
+        });
+      }
+    }
+
+    // Associa os tickets pendentes à ShiftChange
+    for (const ticket of pendentes) {
+      if (ticket.id && ticket.id.trim() !== "") {
+        await prisma.shiftChangePendingTicket.create({
+          data: {
+            shiftChangeId: shiftChange.id,
+            ticketId: ticket.id,
+          },
+        });
+      }
+    }
+
+    return response.status(200).json({
+      message: "Shift Change created successfully",
+      shiftChange: shiftChange,
+    });
+  } catch (err) {
+    console.error("Error on creating Shift Change: ", err);
+    return response
+      .status(500)
+      .json({ message: "Internal server error", error: true });
+  }
 });
 
-router.get('/', async (request: Request, response: Response) => {
-	try {
-		const shiftChanges = await prisma.shiftChange.findMany({
-			include: {
-				ShiftChangeCompanies: true,
-				ResponsibleUser: true,
-				ShiftChangeAssignedTicket: true,
-				ShiftChangePendingTicket: true,
-				ShiftChangePlannedTicket: true,
-			},
-		});
+router.get("/", async (request: Request, response: Response) => {
+  try {
+    const shiftChanges = await prisma.shiftChange.findMany({
+      include: {
+        ShiftChangeCompanies: true,
+        ResponsibleUser: true,
+        ShiftChangeAssignedTicket: {
+          include: {
+            Ticket: {
+              select: {
+                id: true,
+                description: true,
+              },
+            },
+          },
+        },
+        ShiftChangePendingTicket: {
+          include: {
+            Ticket: {
+              select: {
+                id: true,
+                description: true,
+              },
+            },
+          },
+        },
+        ShiftChangePlannedTicket: {
+          include: {
+            Ticket: {
+              select: {
+                id: true,
+                description: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
-		console.log('shiftChanges', shiftChanges);
-
-		return response.status(200).json({
-			message: 'Shift Changes found',
-			body: shiftChanges,
-			error: false,
-		});
-	} catch (err) {
-		return response.status(500).json(err);
-	}
+    return response.status(200).json({
+      message: "Shift Changes found",
+      body: shiftChanges,
+      error: false,
+    });
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 });
 
-router.get('/:id', async (request: Request, response: Response) => {
-	const id = request.params.id;
+router.get("/:id", async (request: Request, response: Response) => {
+  const id = request.params.id;
 
-	try {
-		const shiftChange = await prisma.shiftChange.findUnique({
-			where: { id },
-			include: {
-				ShiftChangeCompanies: true,
-				ResponsibleUser: true,
-				ShiftChangeAssignedTicket: true,
-				ShiftChangePendingTicket: true,
-				ShiftChangePlannedTicket: true,
-			},
-		});
+  try {
+    const shiftChange = await prisma.shiftChange.findUnique({
+      where: { id },
+      include: {
+        ShiftChangeCompanies: true,
+        ResponsibleUser: true,
+        ShiftChangeAssignedTicket: true,
+        ShiftChangePendingTicket: true,
+        ShiftChangePlannedTicket: true,
+      },
+    });
 
-		return response.status(200).json({
-			message: 'Shift Changes found',
-			body: shiftChange,
-			error: false,
-		});
-	} catch (err) {
-		return response.status(500).json(err);
-	}
+    return response.status(200).json({
+      message: "Shift Changes found",
+      body: shiftChange,
+      error: false,
+    });
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 });
 
-router.put('/:id', async (request: Request, response: Response) => {
-	const id = request.params.id;
-	const { date, responsibleUserId, temperatureControl } = request.body;
+router.put("/:id", async (request: Request, response: Response) => {
+  const id = request.params.id;
+  const { date, responsibleUserId, temperatureControl } = request.body;
 
-	try {
-		const shiftChange = await prisma.shiftChange.update({
-			where: { id },
-			data: {
-				date,
-				responsibleUserId,
-				temperatureControl,
-			},
-		});
+  try {
+    const shiftChange = await prisma.shiftChange.update({
+      where: { id },
+      data: {
+        date,
+        responsibleUserId,
+        temperatureControl,
+      },
+    });
 
-		return response.status(200).json({
-			message: 'Shift Change updated successfully',
-			body: shiftChange,
-			error: false,
-		});
-	} catch (err) {
-		return response.status(500).json(err);
-	}
+    return response.status(200).json({
+      message: "Shift Change updated successfully",
+      body: shiftChange,
+      error: false,
+    });
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 });
 
-router.delete('/:id', async (request: Request, response: Response) => {
-	const id = request.params.id;
+router.delete("/:id", async (request: Request, response: Response) => {
+  const id = request.params.id;
 
-	try {
-		await prisma.shiftChange.delete({ where: { id } });
+  try {
+    await prisma.shiftChange.delete({ where: { id } });
 
-		return response.status(200).json({
-			message: 'Shift Changes deleted successfully',
-			error: false,
-		});
-	} catch (err) {
-		return response.status(500).json(err);
-	}
+    return response.status(200).json({
+      message: "Shift Changes deleted successfully",
+      error: false,
+    });
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 });
 
 export default router;
